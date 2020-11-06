d1199fcd7b0fb64553f6dbd9b8f99e429b61d9cf
dikadeantara <70183827+dikadeantara@users.noreply.github.com>
2020-11-07 05:08:53 +0700

    Add files via upload
    Praktikum tahap 4

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html> 
+<html> 
+ <head> 
+ <title>Halaman Dinamis dengan PHP</title>  <link rel="stylesheet" href="style.css">  </head> 
+<body> 
+ <header> 
+ <h2>SMKN 7 BALEENDAH</h2>  
+<p>Membuat Halaman Dinamis dengan  PHP</p> 
+ </header> 
+ <div id="navbar"> 
+ <div class="nav"> 
+ <a href="home.php?home">Home</a>  
+ <a href="article.php?article">Article</a> 
+ <a href="about.php?about">About</a> 
+ <a href="contact.php?contact">Contact</a>  
+ 
+ </div> 
+ </div> 
+ <!-- navbar --> 
+ <div class="konten">
---
+++ home.php
@@ -0,0 +1,2 @@
+<h3>ini adalah halaman Home</h3>
+
